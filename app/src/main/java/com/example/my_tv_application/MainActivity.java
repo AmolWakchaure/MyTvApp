941606471a3a14454ae18766e47f2097ca6b9253
AmolWakchaure <ambi.wakchaure@gmail.com>
2019-01-30 18:01:31 +0530

    test synch on snehal pc

--- app/src/main/java/com/example/my_tv_application/MainActivity.java
+++ app/src/main/java/com/example/my_tv_application/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends Activity {
 
 
     //amol wakchauredf
+    //snehal
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
